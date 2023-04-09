7feb7ebe11db018adbb4e44a88e2b8c53b0f3c94
stephfrankable <112651864+stephfrankable@users.noreply.github.com>
2023-04-09 16:59:46 -0500

    Able to view previously added note

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useReducer } from 'react';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+import { listNotes } from './graphql/queries';
 
-function App() {
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: ''}
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default: 
+      return state
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const fetchNotes = async () => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+    } catch (err) {
+      console.error(err);
+      dispatch({ type: 'ERROR '});
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: { textAlign: 'left' },
+    p: { color: '#1890ff' }
+  }
+
+  function renderItem(item) {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta 
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+       />
     </div>
-  );
+  )
+
 }
 
 export default App;
